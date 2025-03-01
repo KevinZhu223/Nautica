56123c432bd09bc3c701773d8efd4be6fdf2f9c2
ilumn <42507764+ilumn@users.noreply.github.com>
2025-03-01 14:02:16 -0500

    AHHHH

---
+++ app/config/settings.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+import os
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    OPENAI_KEY: str
+    DATABASE_URL: str
+    REGION_NAME: str
+    BUCKET_NAME: str
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
+
+
+settings = Settings()
