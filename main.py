99b258f57b530df69ecb164c369d78aa6a81124d
ilumn <42507764+ilumn@users.noreply.github.com>
2025-03-02 03:46:50 -0500

    fix cors

--- main.py
+++ main.py
@@ -21,11 +21,13 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Add your frontend URL
+    allow_origins=["https://kevinzhu223.github.io"],
+    allow_origin_regex=r"^http://localhost(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # Include API routers
 app.include_router(users.router, prefix="/users", tags=["users"])
